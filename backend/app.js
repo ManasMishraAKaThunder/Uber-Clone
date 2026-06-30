d821153e32f3fa0fd79d29b52de57a86e2d0958d
ManasMishraAKaThunder <panditmanasmishra09@gmail.com>
2026-06-30 19:46:49 +0530

    db setup complete

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
-    const dotenv = require('dotenv');
-    dotenv.config();
-    const express = require('express');
-    const cors = require('cors');
-    const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const connectToDb = require("./db/db");
+connectToDb();
 
 
- app.use(cors());
+app.use(cors());
 
-    app.get('/' , (req, res) => {
-        res.send("hello World");
-    } );
+app.get('/', (req, res) => {
+    res.send("hello World");
+});
 
-    module.exports = app;
+module.exports = app;
 

91f8eaf86e1fcffde0325d16290e1a4549851aff
ManasMishraAKaThunder <panditmanasmishra09@gmail.com>
2026-07-23 10:26:09 +0530

    feat: integrate react-router-dom for routing in the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'   //used rafce for bolier plate 
+import { Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
-      <h1 className="text-3xl font-bold">App</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user-login" element={<UserLogin />} />
+        <Route path="/user-signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<Captainlogin />} />
+        <Route path="/captain-signup" element={<Captainsignup />} />
+      </Routes>
     </div>
   )
 }

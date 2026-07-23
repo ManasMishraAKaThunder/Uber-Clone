f028cddb77dabc74c625122a1fd202787c0bc08f
ManasMishraAKaThunder <panditmanasmishra09@gmail.com>
2026-07-23 18:07:51 +0530

    feat: create home page component with brand imagery and layout styling

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='h-screen w-full bg-indigo-400'>
-      <div>
-        <h2>Get Started With Uber</h2>
-        <button>Continue</button>
+    <div>
+      <div className=' bg-[url("/trafficLight.jpg")]h-screen pt-8 flex justify-between flex-col w-full bg-indigo-400'>
+        <img className='w-16 ml-8' src="/Uber_logo_2018.png" alt="Uber Logo" />
+        <div className='bg-white pb-7 py-4 px-4 flex flex-col gap-5'>
+          <h2 className='text-3xl font-bold'>Get Started With Uber</h2>
+          <button className='w-full bg-black text-white py-3 rounded-md hover:bg-gray-800'>Continue</button>
+        </div>
       </div>
     </div>
   )
